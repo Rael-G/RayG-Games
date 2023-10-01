b152f7f63ae0f872d022a1d80ac74fb6e9f26c13
Israel Albino Galvan <israel.galvan@live.com>
2023-10-01 19:53:54 -0300

    Working VirtualScreen

--- RayG/Window.cs
+++ RayG/Window.cs
@@ -6,27 +6,40 @@ namespace RayG
     {
         //Default Options
         private static string _name = "Default";
-        private static int _width = 1280;
-        private static int _height = 720;
+        private static int _width = 800;
+        private static int _height = 450;
         private static int _virtualWidth = _width;
         private static int _virtualHeight = _height;
-        private static bool _fullscreen = false;
 
-        public static string Name { get { return _name; } set { _name = value; } }
-        public static int Width { get { return _width; } set { _width = value; } }
-        public static int VirtualWidth { get { return _virtualWidth; } set { _virtualWidth = value; } }
-        public static int VirtualHeight { get { return _virtualHeight; } set { _virtualHeight = value; } }
-        public static int Height { get { return _height; } set { _height = value; } }
-        public static bool Fullscreen { get { return _fullscreen; } set { _fullscreen = value; } }
+        public static string Name { get => _name; set => _name = value; }
+        public static int Width { get => Raylib.GetScreenWidth(); set => _width = value; }
+        public static int Height { get => Raylib.GetScreenHeight(); set => _height = value; }
+        public static int VirtualWidth { get => _virtualWidth; set => _virtualWidth = value; }
+        public static int VirtualHeight { get => _virtualHeight; set => _virtualHeight = value; }
+        public static int Scale 
+        { 
+            get 
+            {
+                var scale = (_height + _width) / (VirtualHeight + VirtualWidth);
+                if (scale < 1)
+                {
+                    scale = 1;
+                }
+                return scale;
+            } 
+        }
+
+        public static void ResizeWindow(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            Raylib.SetWindowSize(width, height);
+        }
 
         internal static void Init()
         {
-            Raylib.InitWindow(Width, Height, Name);
-
-            if (Raylib.IsWindowFullscreen() != Fullscreen)
-            {
-                Raylib.ToggleFullscreen();
-            }
+            Raylib.InitWindow(_width, _height, Name);
+            Raylib.SetWindowMinSize(_virtualWidth, _virtualHeight);
         }
 
         internal static void Close()
