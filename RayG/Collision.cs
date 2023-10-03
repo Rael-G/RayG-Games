c1cc83ee4349a16bfbce3d10e997eefde2fbe020
Israel Albino Galvan <israel.galvan@live.com>
2023-10-03 12:57:38 -0300

    Fixed bug in collision

--- RayG/Collision.cs
+++ RayG/Collision.cs
@@ -9,50 +9,22 @@ namespace RayG
 {
     public static class Collisions
     {
+
         public static void Collision(this GameObject gameObject)
         {
-            var colliders = gameObject.CollisionRecursion();
-            foreach (var child in gameObject.Childs)
-            {
-                if (child is ICollisor collisorChild)
-                {
-                    //If a previous colliding object is no more colliding, remove it from 
-                    // its Collisor.Colliders List
-                    for (int i = collisorChild.Collisor.Colliders.Count - 1; i >= 0; i--)
-                    {
-                        var collider = collisorChild.Collisor.Colliders[i];
-                        if (!collisorChild.Collisor.IsColliding(collider))
-                        {
-                            collisorChild.Collisor.Colliders.RemoveAt(i);
-                            collisorChild.OnCollisionExit(collider);
-                        }
-                    }
+            var colliders = gameObject.ColliderDetecter();
 
-                    //Verify if this GameObject.Collisor is Colliding with any
-                    //collider found by CollisionRecursion()
-                    foreach (var collider in colliders)
-                    {
-                        if (collider != collisorChild.Collisor
-                            && !collisorChild.Collisor.Colliders.Contains(collider))
-                        {
-                            if (collisorChild.Collisor.IsColliding(collider))
-                            {
-                                collisorChild.Collisor.Colliders.Add(collider);
-                                collisorChild.OnCollisionEnter(collider);
-                            }
-                        }
-                    }
-                }
-            }
+            gameObject.CollisionCleaner();
+            gameObject.CollisionChecker(colliders);
         }
 
-        //Finds all collisors recursively
-        private static List<Collisor> CollisionRecursion(this GameObject gameObject)
+        //Finds all collisors within this GameObject.Childs recursively
+        private static List<Collisor> ColliderDetecter(this GameObject gameObject)
         {
             List<Collisor> colliders = new();
             foreach (var child in gameObject.Childs)
             {
-                colliders.AddRange(child.CollisionRecursion());
+                colliders.AddRange(child.ColliderDetecter());
 
                 if (child is ICollisor colChild)
                 {
@@ -60,6 +32,53 @@ namespace RayG
                 }
             }
             return colliders;
+        }
+
+        //If a previous colliding object is no more colliding, remove it from 
+        //   Collisor.Colliders and calls OnCollisionExit, ecursively on all Childs
+        private static void CollisionCleaner(this GameObject gameObject )
+        {
+            foreach (var child in gameObject.Childs)
+            {
+                child.CollisionCleaner(); 
+            }
+
+            if (gameObject is ICollisor colObject)
+
+            for (int i = colObject.Collisor.Colliders.Count - 1; i >= 0; i--)
+            {
+                var collider = colObject.Collisor.Colliders[i];
+                if (!colObject.Collisor.IsColliding(collider))
+                {
+                     colObject.Collisor.Colliders.RemoveAt(i);
+                     colObject.OnCollisionExit(collider);
+                }
+            }
+        }
+
+        //Verify if this GameObject.Collisor is Colliding with any
+        //collider found in received Colliders List, recursively on all Childs
+        private static void CollisionChecker(this GameObject gameObject, List<Collisor> colliders )
+        {
+            foreach (var collider in colliders)
+            {
+                foreach (var child in gameObject.Childs)
+                {
+                    child.CollisionChecker(colliders); 
+                }
+
+                if (gameObject is ICollisor colObject)
+
+                if (collider != colObject.Collisor
+                    && !colObject.Collisor.Colliders.Contains(collider))
+                {
+                    if (colObject.Collisor.IsColliding(collider))
+                    {
+                        colObject.Collisor.Colliders.Add(collider);
+                        colObject.OnCollisionEnter(collider);
+                    }
+                }
+            }
         }
     }
 }
