65e4a434bd2d9ad965e0973decd7d303820688ab
Israel Albino Galvan <israel.galvan@live.com>
2023-10-16 10:34:24 -0300

    Breakout better level maker

--- Breakout/GameLogic/LevelMaker.cs
+++ Breakout/GameLogic/LevelMaker.cs
@@ -16,6 +16,9 @@ namespace Breakout.GameLogic
         SoundManager _soundManager;
         SpriteSheet _spriteSheet;
 
+        const int brickWidth = 17;
+        const int brickHeight = 9;
+
         public LevelMaker(SoundManager soundManager, SpriteSheet spriteSheet)
         {
             _spriteSheet = spriteSheet;
@@ -25,12 +28,10 @@ namespace Breakout.GameLogic
         public List<Brick> RandomLevel(int level)
         {
             var bricks = new List<Brick>();
-            var highesTier = Math.Min(SpriteSheet.Strongest, level / 5);
+            var highestTier = Math.Min(SpriteSheet.Strongest, level / 5);
             var highestColor = Math.Min(SpriteSheet.Yellow, level % 5 + 3);
-            var rows = Raylib.GetRandomValue(1, 5);
+            var rows = Raylib.GetRandomValue(3, 5);
             var cols = Raylib.GetRandomValue(7, 15);
-            var brickWidth = 17;
-            var brickHeight = 9;
 
             var padding = Window.VirtualWidth * 0.10f + cols * brickWidth;
             var initialPosX = Window.VirtualWidth * 0.10f + (Window.VirtualWidth * 0.90f - padding) / 2;
@@ -38,12 +39,34 @@ namespace Breakout.GameLogic
 
             for (int i = 0; i < rows; i++)
             {
+                var skipPattern = Raylib.GetRandomValue(0, 1) != 0;
+                var alternatePattern = Raylib.GetRandomValue(0, 1) != 0;
+
+                var skipFlag = Raylib.GetRandomValue(0, 1) != 0;
+                var alternateFlag = Raylib.GetRandomValue(0, 1) != 0;
+
+                var alternateColor1 = Raylib.GetRandomValue(0, highestColor);
+                var alternateColor2 = Raylib.GetRandomValue(0, highestColor);
+                var alternateTier1 = Raylib.GetRandomValue(0, highestTier);
+                var alternateTier2 = Raylib.GetRandomValue(0, highestTier);
+
+                var solidColor = Raylib.GetRandomValue(0, highestColor);
+                var solidTier = Raylib.GetRandomValue(0, highestTier);
+
                 for (int j = 0; j < cols; j++)
                 {
-                    var color = Raylib.GetRandomValue(0, highestColor);
-                    var tier = Raylib.GetRandomValue(0, highesTier);
+                    if (SkipHandler(skipPattern, ref skipFlag))
+                    {
+                        pos.X += brickWidth;
+                        continue;
+                    }
+
+                    ColorTierHandler(out int color, out int tier, alternatePattern, ref alternateFlag,
+                        solidColor, solidTier, alternateColor1, alternateColor2, alternateTier1, alternateTier2);
+                    
                     var brick = new Brick(_soundManager, _spriteSheet.Bricks[color,
                         tier], tier, pos);
+
                     bricks.Add(brick);
                     pos.X += brickWidth;
                 }
@@ -52,6 +75,45 @@ namespace Breakout.GameLogic
                 pos.Y += brickHeight;
             }
             return bricks;
+        }
+
+        private static bool SkipHandler(bool skipPattern, ref bool skipFlag)
+        {
+            if (skipPattern && skipFlag)
+            {
+                skipFlag = false;
+                return false;
+            }
+            else if (skipPattern)
+            {
+                skipFlag = true;
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void ColorTierHandler(out int color, out int tier, bool alternatePattern, 
+            ref bool alternateFlag, int solidColor, int solidTier, int alternateColor1, int alternateColor2,
+            int alternateTier1, int alternateTier2) 
+        {
+            if (alternatePattern && alternateFlag)
+            {
+                alternateFlag = false;
+                color = alternateColor1;
+                tier = alternateTier1;
+            }
+            else if (alternatePattern)
+            {
+                alternateFlag = true;
+                color = alternateColor2;
+                tier = alternateTier2;
+            }
+            else
+            {
+                color = solidColor;
+                tier = solidTier;
+            }
         }
     }
 }
