c92b8990c5220f46c2861f36fe48e7409d686332
Israel Albino Galvan <israel.galvan@live.com>
2023-10-30 11:51:49 -0300

    Class to handle with async operations

---
+++ RayG/Utils/Timing.cs
@@ -0,0 +1,47 @@
+﻿namespace RayG
+{
+    public class Timing
+    {
+        public static async Task EveryAsync(int interval, Action action)
+        {
+            interval *= 1000;
+            while (true)
+            {
+                await Task.Delay(interval);
+                action();
+            }
+        }
+
+        public static async Task AfterAsync(int interval, Action action)
+        {
+            interval *= 1000;
+            await Task.Delay(interval);
+            action();
+        }
+
+        public static async Task InterpolateAsync(int duration, float from, float to, Action<float> target)
+        {
+            var distance = to - from;
+            double currentTime = 0;
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+            while (currentTime < duration)
+            {
+                await Task.Delay(1);
+                currentTime = stopwatch.Elapsed.TotalSeconds;
+                var t = (float)currentTime / duration;
+                var value = from + t * distance;
+                target(value);
+            }
+        }
+
+        public static async void ChainAsync(IEnumerable<Func<Task>> tasks)
+        {
+            foreach (var funcTask in tasks)
+            {
+                var task = funcTask();
+                await task;
+            }
+        }
+    }
+}
