d5e1382725885300f2e5d68344ad1848b860e6c0
Israel Albino Galvan <israel.galvan@live.com>
2023-10-30 11:50:34 -0300

    SpriteSheet is handled by RayG now

---
+++ RayG/Resources/SpriteSheet.cs
@@ -0,0 +1,75 @@
+﻿using Raylib_cs;
+
+namespace RayG
+{
+    public class SpriteSheet : GameObject
+    {
+        readonly Texture2D _texture;
+
+        public SpriteSheet(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public override void Awake()
+        {
+            base.Awake();
+        }
+
+        public Sprite[] ArrangeArrayEqual(int quantity, int width, int height, int scale = 1)
+        {
+            Sprite[] sprites = new Sprite[quantity];
+            Sprite sprite;
+            Rectangle source;
+            int srcX = 0;
+            int srcY = 0;
+
+            for (int i = 0; i < quantity; i++)
+            {
+                if (srcX >= _texture.width)
+                {
+                    srcX = 0;
+                    srcY += height;
+                    if (srcY >= _texture.height)
+                        break;
+                }
+
+                source = new Rectangle(srcX, srcY, width, height);
+                sprite = new Sprite(_texture, source, scale);
+                sprites[i] = sprite;
+                srcX += width;
+            }
+
+            return sprites;
+        }
+
+        public Sprite[,] ArrangeMatrixEqual(int rows, int columns, int width, int height, int scale = 1)
+        {
+            Sprite[,] sprites = new Sprite[rows, columns];
+            Sprite sprite;
+            Rectangle source;
+            int srcX;
+            int srcY = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                srcX = 0;
+                for (int j = 0; j < columns; j++)
+                {
+                    if (srcX >= _texture.width)
+                        break;
+
+                    source = new Rectangle(srcX, srcY, width, height);
+                    sprite = new Sprite(_texture, source, scale);
+                    sprites[i,j] = sprite;
+                    srcX += width;
+                }
+                srcY += height;
+                if (srcY >= _texture.height)
+                    break;
+            }
+
+            return sprites;
+        }
+    }
+}
---
+++ RayG/Resources/Structs/Sprite.cs
@@ -0,0 +1,46 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace RayG
+{
+    public struct Sprite
+    {
+        private int _scale;
+        private bool _centerAxis;
+
+        public Texture2D Texture { get; private set; }
+        public int Width { get => (int)Source.width; }
+        public int Height { get => (int)Source.height;  }
+        public Rectangle Source { get; private set; }
+        public Vector2 Axis { get; set; }
+        public int Scale
+        {
+            get => _scale;
+            set
+            {
+                _scale = value;
+                SetAxis();
+            }
+        }
+
+        public Sprite(Texture2D texture, Rectangle source, int scale, bool centerAxis = false)
+        {
+            Texture = texture;
+            Source = source;
+            _centerAxis = centerAxis;
+            Scale = scale;
+        }
+
+        private void SetAxis()
+        {
+            if (_centerAxis)
+            {
+                Axis = new(Source.width * Scale / 2, Source.height * Scale / 2);
+            }
+            else
+            {
+                Axis = new(0, 0);
+            }
+        }
+    }
+}
